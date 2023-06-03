cae9bee5249fe3de596c185d494d57dbc378a815
cleverBucket <127633273+cleverBucket@users.noreply.github.com>
2023-06-03 07:35:17 -0700

    underlined div based on text input. no underline on spaces and dashes. Dashes show. No letters show.

--- script.js
+++ script.js
@@ -6,22 +6,43 @@ function stringToArrayHandler(event) {
   let string = document.getElementById('phrase').value;
   let letterArray = [];
   letterArray = string.split("");
-  console.log(letterArray[0]);
+  console.log(letterArray);
   console.log(letterArray[3]);
   ArrayToDivCreation(letterArray);
 }
 
+
+
+
+
 function ArrayToDivCreation(array) {
-  for(const element of array) {
+  for(const indexContent of array) {
+   
     const newDiv = document.createElement("div");
-    const newContent = document.createTextNode(element);
+   
+    
     newDiv.className = "letterBlock";
+    if(indexContent === " " || indexContent === "-"){
+    newDiv.style.borderBottom = "none";
+    console.log(indexContent);
+    }
+   
+    if(indexContent === "-") {
+    const newContent = document.createTextNode(indexContent);
     newDiv.appendChild(newContent);
+    newDiv.style.paddingBottom='10px';
+    }
     const currentDiv = document.getElementById("letters");
     currentDiv.appendChild(newDiv);
     document.getElementById('button').removeEventListener('click', stringToArrayHandler);
+
+
+    
+
+  
     
   }
+  
 }
 
 
