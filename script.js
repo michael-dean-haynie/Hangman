a341229aa6b858b200c33d5ecd4101a5aca74337
cleverBucket <127633273+cleverBucket@users.noreply.github.com>
2023-06-03 16:18:07 -0700

    choose letter shows up on correct line below

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
+/*Choose Word*/
 document.getElementById('button').addEventListener('click', stringToArrayHandler);
+let letterArray = [];
 
 function stringToArrayHandler(event) {
 
   let string = document.getElementById('phrase').value;
-  let letterArray = [];
+  
   letterArray = string.split("");
   console.log(letterArray);
   console.log(letterArray[3]);
   ArrayToDivCreation(letterArray);
 }
-
-
-
-
 
 function ArrayToDivCreation(array) {
   for(const indexContent of array) {
@@ -42,19 +39,60 @@ function ArrayToDivCreation(array) {
     currentDiv.appendChild(newDiv);
     document.getElementById('button').removeEventListener('click', stringToArrayHandler);
 
-
-    
-
-  
-    
   }
   
 }
 
+/*Choose Letter*/
+
+document.getElementById('A').addEventListener('click', chooseLetterHandler);
+document.getElementById('B').addEventListener('click', chooseLetterHandler);
+document.getElementById('C').addEventListener('click', chooseLetterHandler);
+document.getElementById('D').addEventListener('click', chooseLetterHandler);
+document.getElementById('E').addEventListener('click', chooseLetterHandler);
+document.getElementById('F').addEventListener('click', chooseLetterHandler);
+document.getElementById('G').addEventListener('click', chooseLetterHandler);
+document.getElementById('H').addEventListener('click', chooseLetterHandler);
+document.getElementById('I').addEventListener('click', chooseLetterHandler);
+document.getElementById('J').addEventListener('click', chooseLetterHandler);
+document.getElementById('K').addEventListener('click', chooseLetterHandler);
+document.getElementById('L').addEventListener('click', chooseLetterHandler);
+document.getElementById('M').addEventListener('click', chooseLetterHandler);
+document.getElementById('N').addEventListener('click', chooseLetterHandler);
+document.getElementById('O').addEventListener('click', chooseLetterHandler);
+document.getElementById('P').addEventListener('click', chooseLetterHandler);
+document.getElementById('Q').addEventListener('click', chooseLetterHandler);
+document.getElementById('R').addEventListener('click', chooseLetterHandler);
+document.getElementById('S').addEventListener('click', chooseLetterHandler);
+document.getElementById('T').addEventListener('click', chooseLetterHandler);
+document.getElementById('U').addEventListener('click', chooseLetterHandler);
+document.getElementById('V').addEventListener('click', chooseLetterHandler);
+document.getElementById('W').addEventListener('click', chooseLetterHandler);
+document.getElementById('X').addEventListener('click', chooseLetterHandler);
+document.getElementById('Y').addEventListener('click', chooseLetterHandler);
+document.getElementById('Z').addEventListener('click', chooseLetterHandler);
 
 
+ function chooseLetterHandler(event) {
+   event.target.style.backgroundColor='grey';
+   
+   let chosenLetter = document.getElementById(event.target.id)
+   let lookForInArray = chosenLetter.innerHTML;
+   console.log(lookForInArray);
+
+   for (let i=0; i < letterArray.length; i++) {
+    if (letterArray[i] === lookForInArray) {
+      //let selector = "#letters :nth-child(" + (i+1) + ")";
+      let elem = document.querySelector("#letters :nth-child(" + (i+1) + ")");
+      elem.innerHTML=lookForInArray;
+      elem.classList.remove("letterBlock");
+    }
+   }
+   
+   chosenLetter.removeEventListener('click', chooseLetterHandler);
+   
+   
+  }
 
 
-
- 
   
